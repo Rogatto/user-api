0b52b06e151535e7cdab42d7a9b549983eaff68a
Guilherme Rogatto <gui.rogatto@gmail.com>
2022-03-18 11:11:02 -0300

    improving allure report and automation

--- src/test/java/com/app/crud/integration/TestUserIT.java
+++ src/test/java/com/app/crud/integration/TestUserIT.java
@@ -1,12 +1,10 @@
 package com.app.crud.integration;
 
-import com.app.crud.integration.request.AuthSigninDTO;
 import com.jayway.restassured.response.Response;
 import io.qameta.allure.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import static com.jayway.restassured.RestAssured.given;
+import org.springframework.beans.factory.annotation.Value;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Epic("User API")
@@ -14,91 +12,27 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class TestUserIT extends TestConfigurationIT{
 
 
+    @Value("${server.port}")
+    private int applicationPort;
+
+    private static final String applicationHost = "http://localhost:";
+
     @BeforeAll
     static void initAll() {
     }
 
-
     @Test
-    @Step("[CT-XXX] Add here name of test scenario")
-    @Description("any description")
+    @Step("[CT-XXX] Executing all user resources")
+    @Description("Testing all user's resources from scratch")
     @Severity(SeverityLevel.CRITICAL)
-    void testAddUser(){
-
-        AuthSigninDTO authSigninDTO = AuthSigninDTO.builder()
-                .username("admin")
-                .password("password")
-                .build();
-
-        Response signinResponse = given()
-                .contentType("application/json")
-                .body(authSigninDTO)
-                .when()
-                .post("http://localhost:8080/auth/signin")
-                .then()
-                .extract()
-                .response();
-
-        signinResponse.getBody().prettyPrint();
+    void testAllUserResources(){
 
 
-        Response getUserMeResponse = given()
-                .header("Authorization", "Bearer " + signinResponse.getBody().jsonPath().getString("token"))
-                .when()
-                .get("http://localhost:8080/api/users/me")
-                .then()
-                .extract()
-                .response();
-
-        getUserMeResponse.getBody().prettyPrint();
-
-
-        Response postUserResponse = given()
-                .header("Authorization", "Bearer " + signinResponse.getBody().jsonPath().getString("token"))
-                .contentType("application/json")
-                .body("{\n    \"name\": \"simple test 1\",\n    \"username\": \"test3\",\n    \"password\": \"{bcrypt}$2a$10$kavSouNY7FWP2TpIPwedNOvNX4BXjBkXTG5fk71zWmX/4.yH2oC1O\",\n    \"roles\": [\n                \"ROLE_USER\"\n            ],\n    \"createdAt\": \"2022-03-14T13:18:58.651Z\",\n    \"enabled\": true,\n    \"accountNonExpired\": true,\n    \"accountNonLocked\": true,\n    \"credentialsNonExpired\": true\n}")
-                .when()
-                .post("http://localhost:8080/api/users")
-                .then()
-                .extract()
-                .response();
-
-        postUserResponse.getBody().prettyPrint();
-        System.out.println("return code status was: " + postUserResponse.getStatusCode());
-
-        Response getUser = given()
-                .when()
-                .get( "http://localhost:8080/api/users")
-                .then()
-                .extract()
-                .response();
-
-        getUser.getBody().prettyPrint();
-
-
-        Response getUserByIdResponse = given()
-                .header("Authorization", "Bearer " + signinResponse.getBody().jsonPath().getString("token"))
-                .when()
-                .get(postUserResponse.getHeader("Location"))
-                .then()
-                .extract()
-                .response();
-
-        getUserByIdResponse.getBody().prettyPrint();
-        System.out.println("return code status was: " + getUserByIdResponse.getStatusCode());
-
-        System.out.println("Header Location returned from post user: " + postUserResponse.getHeader("Location"));
-        /*Response deleteUserResponse = given()
-                .header("Authorization", "Bearer " + signinResponse.getBody().jsonPath().getString("token"))
-                .contentType("application/json")
-                .when()
-                .delete(postUserResponse.getHeader("Location"))
-                .then()
-                .extract()
-                .response();
-
-        deleteUserResponse.getBody().prettyPrint();
-        System.out.println("return code status was: " + deleteUserResponse.getStatusCode());*/
+        Response signinResponse = UserSteps.postUserSignin("admin", "password", applicationHost + applicationPort);
+        UserSteps.getUserMe(signinResponse.getBody().jsonPath().getString("token"), applicationHost + applicationPort);
+        Response postUserResponse = UserSteps.postUser(signinResponse.getBody().jsonPath().getString("token"), applicationHost + applicationPort);
+        UserSteps.getAllUsers(applicationHost + applicationPort);
+        UserSteps.getUserById(signinResponse.getBody().jsonPath().getString("token"), postUserResponse.getHeader("Location"));
 
         assertTrue(true);
     }
---
+++ src/test/java/com/app/crud/integration/UserSteps.java
@@ -0,0 +1,116 @@
+package com.app.crud.integration;
+
+import com.app.crud.integration.request.AuthSigninDTO;
+import com.jayway.restassured.response.Response;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
+
+import static com.jayway.restassured.RestAssured.given;
+
+public class UserSteps {
+
+
+    @Step("User signin")
+    public static Response postUserSignin(String username, String password, String baseUrl) {
+        /*user sign in */
+
+        AuthSigninDTO authSigninDTO = AuthSigninDTO.builder()
+                .username(username)
+                .password(password)
+                .build();
+
+        Response signinResponse = given()
+                .contentType("application/json")
+                .body(authSigninDTO)
+                .when()
+                .post(baseUrl + "/auth/signin")
+                .then()
+                .extract()
+                .response();
+
+        signinResponse.getBody().prettyPrint();
+        Allure.addAttachment("auth/signin response body was: ", signinResponse.getBody().asString());
+        Allure.addAttachment("auth/signin status code was: ", String.valueOf(signinResponse.getStatusCode()));
+
+        return signinResponse;
+    }
+
+
+
+    @Step("GET All Users")
+    public static Response getAllUsers(String baseUrl) {
+
+        Response getAllUsers = given()
+                .when()
+                .get( baseUrl + "/api/users")
+                .then()
+                .extract()
+                .response();
+
+        getAllUsers.getBody().prettyPrint();
+        Allure.addAttachment("Get all users response body was: ", getAllUsers.getBody().asString());
+        Allure.addAttachment("Get all users status code was: ", String.valueOf(getAllUsers.getStatusCode()));
+
+        return getAllUsers;
+    }
+
+
+    @Step("GET User me")
+    public static Response getUserMe(String token, String baseUrl) {
+
+        Response getUserMeResponse = given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get(baseUrl + "/api/users/me")
+                .then()
+                .extract()
+                .response();
+
+        getUserMeResponse.getBody().prettyPrint();
+        Allure.addAttachment("Get user me response body was: ", getUserMeResponse.getBody().asString());
+        Allure.addAttachment("Get user me status code was: ", String.valueOf(getUserMeResponse.getStatusCode()));
+
+        return getUserMeResponse;
+    }
+
+
+    @Step("POST User")
+    public static Response postUser(String token, String baseUrl) {
+
+        Response postUserResponse = given()
+                .header("Authorization", "Bearer " + token)
+                .contentType("application/json")
+                .body("{\n    \"name\": \"simple test 1\",\n    \"username\": \"test3\",\n    \"password\": \"{bcrypt}$2a$10$kavSouNY7FWP2TpIPwedNOvNX4BXjBkXTG5fk71zWmX/4.yH2oC1O\",\n    \"roles\": [\n                \"ROLE_USER\"\n            ],\n    \"createdAt\": \"2022-03-14T13:18:58.651Z\",\n    \"enabled\": true,\n    \"accountNonExpired\": true,\n    \"accountNonLocked\": true,\n    \"credentialsNonExpired\": true\n}")
+                .when()
+                .post(baseUrl + "/api/users")
+                .then()
+                .extract()
+                .response();
+
+        postUserResponse.getBody().prettyPrint();
+        Allure.addAttachment("Post user header location: ", postUserResponse.getHeader("Location"));
+        Allure.addAttachment("Post user status code was: ", String.valueOf(postUserResponse.getStatusCode()));
+
+        return postUserResponse;
+    }
+
+
+    @Step("GET User by ID")
+    public static Response getUserById(String token, String url) {
+
+        Response getUserByIdResponse = given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get(url)
+                .then()
+                .extract()
+                .response();
+
+        getUserByIdResponse.getBody().prettyPrint();
+
+        Allure.addAttachment("Get user by id response body was: ", getUserByIdResponse.getBody().asString());
+        Allure.addAttachment("Get user by id status code was: ", String.valueOf(getUserByIdResponse.getStatusCode()));
+
+        return getUserByIdResponse;
+    }
+}
